55fdc0bd3dcf323a6bc6b001ca5f5b3b4ffd233f
kitakou0313 <kitakou0313@gmail.com>
2021-07-30 14:54:35 +0000

    Add sample of future promise pattern

---
+++ chap14/futurePromise.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func readFile(path string) chan string {
+	promise := make(chan string)
+	go func() {
+		content, err := ioutil.ReadFile(path)
+
+		if err != nil {
+			fmt.Println("Error!")
+			close(promise)
+		} else {
+			promise <- string(content)
+		}
+	}()
+	return promise
+}
+
+func printFunc(futureSrc chan string) chan []string {
+	promise := make(chan []string)
+	go func() {
+		var result []string
+
+		for _, line := range strings.Split(<-futureSrc, "\n") {
+			if strings.HasPrefix(line, "func ") {
+				result = append(result, line)
+			}
+
+		}
+	}()
+	return promise
+
+}
+
+func main() {
+	futureSrc := readFile("futurePromise.go")
+	futureFuncs := printFunc(futureSrc)
+	fmt.Println(strings.Join(<-futureFuncs, "\n"))
+}
